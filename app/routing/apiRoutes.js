30a63bba5e215fe082d6c086a91e3eecc521a2ad
Mark Iudice <markiudice@Marks-MacBook-Pro.local>
2018-04-21 09:21:09 -0400

    missed a save

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,11 @@
 var friends = require('../data/friends.js');
 
-module.exports = (app) => {
-	app.get('/api/friends', (req, res) => {
+module.exports = function(app) {
+	app.get('/api/friends', function(req, res) {
 		res.json(friends);
-	})
+	});
 
-	app.post('/api/friends', (req, res) => {
+	app.post('/api/friends', function(req, res) {
 		friends.push(req.body);
 	})
 };
